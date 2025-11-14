a914f00618463a5993f3dbf452c9647b0a084705
CristinaVoda <vodacristina7@gmail.com>
2025-11-14 23:38:25 +0100

    coockies

--- src/components/CookiesBanner.vue
+++ src/components/CookiesBanner.vue
@@ -61,7 +61,7 @@ export default {
   box-shadow: 0 8px 35px rgba(0, 0, 0, 0.35);
   border: 1px solid rgba(255, 255, 255, 0.18);
 
-  color: #fff;
+  color: #444242;
   font-size: 0.9rem;
   z-index: 9999;
 
@@ -86,13 +86,13 @@ export default {
 }
 
 .accept {
-  background: #00d27f;
+  background: #808382;
   color: #fff;
 }
 
 .reject {
-  background: rgba(255, 255, 255, 0.2);
-  color: #fff;
+  background: rgba(159, 162, 170, 0.2);
+  color: #2f4a6d;
 }
 
 .settings {
